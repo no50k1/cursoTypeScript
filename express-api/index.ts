44e711b1a57066fc35129ffe3ace08eeddd455d7
Cesar Bernaola <bernaolacesar@gmail.com>
2022-07-25 00:36:06 -0500

    Termino del curso Typescript

---
+++ express-api/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+//const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+
+  res.status(201).json({
+    ok: true,
+    msg: 'Nuevo id: 131'
+  });
+
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
